61febada67f90f73f29dd31d64d32f5c152682fb
hmeshram21 <harsh.meshram.ext@holcim.com>
2025-09-26 15:04:23 +0530

    fix env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyDOe1gPO-vRzGldW67uW2BurqwQbkJxe9w",
-  authDomain: "practice-auth-app-6a9e8.firebaseapp.com",
-  projectId: "practice-auth-app-6a9e8",
-  storageBucket: "practice-auth-app-6a9e8.firebasestorage.app",
-  messagingSenderId: "488788681646",
-  appId: "1:488788681646:web:468d52d53a42a4df3d5e3f",
-  measurementId: "G-W2HMSXGC22"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+console.log("Firebase API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
